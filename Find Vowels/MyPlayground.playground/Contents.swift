cea693572f8847362efb39d01aa2d0487d6f7c78
Sachin Daingade <sbdaingade@gmail.com>
2024-06-10 12:52:57 +0530

    race condition added

--- Find Vowels/MyPlayground.playground/Contents.swift
+++ Find Vowels/MyPlayground.playground/Contents.swift
@@ -4,6 +4,7 @@ var greeting = "Hello, playground"
 let vowels = ["a","e", "i","o","u"]
 
 
+
 func findVowelCount(strData: String) -> Int {
     var count = 0
     let newChar = strData.compactMap{$0}
@@ -16,3 +17,4 @@ func findVowelCount(strData: String) -> Int {
     return count
 }
 findVowelCount(strData: greeting)
+let rev = 
